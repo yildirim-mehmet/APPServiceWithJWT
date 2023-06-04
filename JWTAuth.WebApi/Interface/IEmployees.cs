837ecff9ab1d8aa5f09f882b53cf3a754269c834
verigrup <omurga@msn.com>
2023-06-04 10:07:02 +0300

    Add project files.

---
+++ JWTAuth.WebApi/Interface/IEmployees.cs
@@ -0,0 +1,19 @@
+﻿using APP.Service.Models;
+
+namespace APP.Service.Interface
+{
+    public interface IEmployees
+    {
+        public List<Employee> GetEmployeeDetails();
+
+        public Employee GetEmployeeDetails(int id);
+
+        public void AddEmployee(Employee employee);
+
+        public void UpdateEmployee(Employee employee);
+
+        public Employee DeleteEmployee(int id);
+
+        public bool CheckEmployee(int id);
+    }
+}
